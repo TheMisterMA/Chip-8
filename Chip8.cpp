a43ffab8194856f245c96bc835e0d9d4d37bdca0
MrMA <miacel159@gmail.com>
2021-03-12 15:21:17 +0200

    Added the needed code.

--- Chip8.cpp
+++ Chip8.cpp
@@ -3,13 +3,22 @@
 
 #include "Chip8.h"
 
-Chip8::Chip8(const char* file_path)
+Chip8::Chip8(const char *file_path) : //  Initializing randomness seed.
+                                      randGen(std::chrono::system_clock::now().time_since_epoch().count()),
+                                      randByte(std::uniform_int_distribution<uint8_t>(0, 255U)),
+
+                                      //  Initializing `Program Counter` to the starting adresss.
+                                      pc(this->START_ADDRESS)
 {
-    //  Initializing `Program Counter` to the starting adresss.
-    this->pc = this->START_ADDRESS;
 
     //  Loading the program into the emulated ROM
     this->LoadROM(file_path);
+
+    //  Load fonts into memory.
+    for (size_t i = this->FONTSET_START_ADDRESS; i < (this->FONT_HIGHT * this->AMOUNT_OF_CHARS); i++)
+    {
+        this->memory[i] = this->fontset[i - this->FONTSET_START_ADDRESS];
+    }
 }
 
 Chip8::~Chip8()
@@ -23,19 +32,19 @@ Chip8::~Chip8()
  *
  * @param file_path The file containing the program this program will emulate.
  */
-void Chip8::LoadROM(const char* file_path)
+void Chip8::LoadROM(const char *file_path)
 {
     //  Opening the file as a binary stream and set the file pointer at the end.
     std::ifstream code_file(file_path, std::ios::binary | std::ios::ate);
 
-    if(code_file.is_open())
+    if (code_file.is_open())
     {
         //  Get the current read position and assining it as the file size. Allocating a buffer to hold the contents of the file.
         std::streampos file_size = code_file.tellg();
-        char* buffer = new char[file_size];
+        char *buffer = new char[file_size];
 
         //  Check if the memory size allocated for commands has enogh space for all the commands.
-        if(file_size > (this->RAM_SIZE_IN_BYTES - this->START_ADDRESS))
+        if (file_size > (this->RAM_SIZE_IN_BYTES - this->START_ADDRESS))
         {
             printf("Aborting, `ROM` file is bigger than allocated memory buffer.\n");
             throw std::overflow_error("ROM is bigger than allocated memory buffer.");
@@ -47,7 +56,7 @@ void Chip8::LoadROM(const char* file_path)
         code_file.close();
 
         //  Loading the buffer contents into the `Chip-8` memory, starting at 0x200.
-        for(int unsigned i = 0; i < file_size; ++i)
+        for (int unsigned i = 0; i < file_size; ++i)
         {
             memory[this->START_ADDRESS + i] = buffer[i];
         }
--- Chip8.h
+++ Chip8.h
@@ -4,36 +4,66 @@
 #include <cstdint>
 #include <fstream>
 #include <stdexcept>
+#include <chrono>
+#include <random>
 
 class Chip8
 {
 private:
-    static const unsigned int BASE_REG_AMOUNT   = 16;
+    static const unsigned int BASE_REG_AMOUNT = 16;
     static const unsigned int RAM_SIZE_IN_BYTES = 4096;
-    static const unsigned int STACK_LEVELS      = 16;
-    static const unsigned int AMOUNT_OF_CHARS   = 16;
-    static const unsigned int VIDEO_WIDTH       = 64;
-    static const unsigned int VIDEO_HIGHT       = 32;
+    static const unsigned int STACK_LEVELS = 16;
+    static const unsigned int VIDEO_WIDTH = 64;
+    static const unsigned int VIDEO_HIGHT = 32;
 
     //  The starting address, which the `Program Counter` would be initialize to
-    static const unsigned int START_ADDRESS     = 0x200;
+    static const unsigned int START_ADDRESS = 0x200;
 
-    uint8_t     registers[BASE_REG_AMOUNT]{};
-	uint8_t     memory[RAM_SIZE_IN_BYTES]{};
-	uint16_t    index{};
-	uint16_t    pc{};
-	uint16_t    stack[STACK_LEVELS]{};
-	uint8_t     sp{};
-	uint8_t     delayTimer{};
-	uint8_t     soundTimer{};
-	uint8_t     keypad[AMOUNT_OF_CHARS]{};
-	uint32_t    video[VIDEO_WIDTH * VIDEO_HIGHT]{};
-	uint16_t    opcode;
+    //  Characters Font hight and the number of each character.
+    static const unsigned int AMOUNT_OF_CHARS = 16;
+    static const unsigned int FONT_HIGHT = 5;
+    static const unsigned int FONTSET_START_ADDRESS = 0x50;
 
+    const uint8_t fontset[AMOUNT_OF_CHARS * FONT_HIGHT] =
+    {
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+
+    };
+
+    uint8_t registers[BASE_REG_AMOUNT]{};
+    uint8_t memory[RAM_SIZE_IN_BYTES]{};
+    uint16_t index{};
+    uint16_t pc{};
+    uint16_t stack[STACK_LEVELS]{};
+    uint8_t sp{};
+    uint8_t delayTimer{};
+    uint8_t soundTimer{};
+    uint8_t keypad[AMOUNT_OF_CHARS]{};
+    uint32_t video[VIDEO_WIDTH * VIDEO_HIGHT]{};
+    uint16_t opcode;
+
+    //  Randomness seeds
+    std::default_random_engine randGen;
+    std::uniform_int_distribution<uint8_t> randByte;
 public:
-    Chip8(const char* file_path);
+    Chip8(const char *file_path);
     ~Chip8();
 
     //  Loads the binary values in the file to the Memory attribute.
-    void LoadROM(const char* file_path);
+    void LoadROM(const char *file_path);
 };
