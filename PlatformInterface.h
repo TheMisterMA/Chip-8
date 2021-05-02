b1b326273c7abf45b0dc45561e145cd75dbe50eb
MrMA <miacel159@gmail.com>
2021-05-02 17:36:22 +0300

    Added platform interface enabling useage of graphics and keys.

---
+++ PlatformInterface.h
@@ -0,0 +1,24 @@
+#include <SDL.h>
+#include <iostream>
+
+class PlatformInterface
+{
+private:
+    SDL_Window *window{};
+    SDL_Renderer *renderer{};
+    SDL_Texture *texture{};
+
+public:
+    PlatformInterface(const char *title,
+             size_t windowWidth,
+             size_t windowHeight,
+             size_t textureWidth,
+             size_t textureHeight);
+
+    ~PlatformInterface();
+
+    void Update(void const *buffer,
+                int pitch);
+
+    bool ProcessInput(uint8_t *keys);
+}
